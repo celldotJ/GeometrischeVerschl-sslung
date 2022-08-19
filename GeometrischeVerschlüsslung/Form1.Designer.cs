82c08ac2d26cb64d6fc81ff537416292d6cd0c3f
celldotJ <78538876+celldotJ@users.noreply.github.com>
2022-08-19 20:23:31 +0200

    Initial commit

---
+++ GeometrischeVerschlüsslung/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace GeometrischeVerschlüsslung
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTxtBx = new System.Windows.Forms.TextBox();
+            this.encryptBtn = new System.Windows.Forms.Button();
+            this.decryptBtn = new System.Windows.Forms.Button();
+            this.resultLbl = new System.Windows.Forms.Label();
+            this.resultTxtBx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTxtBx
+            // 
+            this.inputTxtBx.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputTxtBx.Location = new System.Drawing.Point(12, 12);
+            this.inputTxtBx.Name = "inputTxtBx";
+            this.inputTxtBx.Size = new System.Drawing.Size(849, 93);
+            this.inputTxtBx.TabIndex = 0;
+            this.inputTxtBx.Text = "SPIONIERENDESPIONE";
+            // 
+            // encryptBtn
+            // 
+            this.encryptBtn.Location = new System.Drawing.Point(867, 12);
+            this.encryptBtn.Name = "encryptBtn";
+            this.encryptBtn.Size = new System.Drawing.Size(150, 40);
+            this.encryptBtn.TabIndex = 1;
+            this.encryptBtn.Text = "Encrypt";
+            this.encryptBtn.UseVisualStyleBackColor = true;
+            this.encryptBtn.Click += new System.EventHandler(this.encryptBtn_Click);
+            // 
+            // decryptBtn
+            // 
+            this.decryptBtn.Location = new System.Drawing.Point(867, 65);
+            this.decryptBtn.Name = "decryptBtn";
+            this.decryptBtn.Size = new System.Drawing.Size(150, 40);
+            this.decryptBtn.TabIndex = 2;
+            this.decryptBtn.Text = "Decrypt";
+            this.decryptBtn.UseVisualStyleBackColor = true;
+            this.decryptBtn.Click += new System.EventHandler(this.decryptBtn_Click);
+            // 
+            // resultLbl
+            // 
+            this.resultLbl.AutoSize = true;
+            this.resultLbl.Location = new System.Drawing.Point(12, 117);
+            this.resultLbl.Name = "resultLbl";
+            this.resultLbl.Size = new System.Drawing.Size(0, 32);
+            this.resultLbl.TabIndex = 3;
+            // 
+            // resultTxtBx
+            // 
+            this.resultTxtBx.Location = new System.Drawing.Point(12, 117);
+            this.resultTxtBx.Name = "resultTxtBx";
+            this.resultTxtBx.Size = new System.Drawing.Size(1005, 39);
+            this.resultTxtBx.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 167);
+            this.Controls.Add(this.resultTxtBx);
+            this.Controls.Add(this.resultLbl);
+            this.Controls.Add(this.decryptBtn);
+            this.Controls.Add(this.encryptBtn);
+            this.Controls.Add(this.inputTxtBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox inputTxtBx;
+        private Button encryptBtn;
+        private Button decryptBtn;
+        private Label resultLbl;
+        private TextBox resultTxtBx;
+    }
+}
---
+++ GeometrischeVerschlüsslung/Form1.cs
@@ -0,0 +1,119 @@
+namespace GeometrischeVerschlüsslung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void encryptBtn_Click(object sender, EventArgs e)
+        {
+            string input = inputTxtBx.Text;
+            input = input.Replace(" ", "").ToUpper();
+            string result = Encrypt(input);
+
+            resultTxtBx.Text = result;
+        }
+
+        private static string Encrypt(string input)
+        {
+            int columns;
+            int rows;
+
+            columns = (int)Math.Ceiling(Math.Sqrt(input.Length));
+            rows = (int)Math.Ceiling((decimal)input.Length / columns);
+
+            List<char[]> Grid = CreateGrid(input, columns, rows);
+
+            string result = GetEncryptedValueFromGrid(columns, Grid);
+            return result;
+        }
+
+        /// <summary>
+        /// Creates the Grid used for encryption.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="columns"></param>
+        /// <param name="rows"></param>
+        /// <returns></returns>
+        private static List<char[]> CreateGrid(string input, int columns, int rows)
+        {
+            List<char[]> Grid = new List<char[]> { };
+            char[] Chars = input.ToCharArray();
+            int progress = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                char[] row = FillGridRow(input, columns, Chars, ref progress);
+                Grid.Add(row);
+            }
+
+            return Grid;
+        }
+
+        /// <summary>
+        /// Fills a <see cref="char[]"/> with parts of the input string 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="columns"></param>
+        /// <param name="Chars"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        private static char[] FillGridRow(string input, int columns, char[] Chars, ref int progress)
+        {
+            char[] row = new char[columns];
+            for (int z = 0; z < columns; z++)
+            {
+                if (progress > input.Length - 1)
+                {
+                    break;
+                }
+                row[z] = Chars[progress];
+                progress++;
+            }
+            if (row.Contains('\0'))
+            {
+                for (int t = 0; t < row.Length; t++)
+                {
+                    if (row[t] == '\0')
+                    {
+                        row[t] = 'X';
+                    }
+                }
+            }
+
+            return row;
+        }
+        /// <summary>
+        /// Reads the encrypted version of the input from the <paramref name="Grid"/>.
+        /// </summary>
+        /// <param name="columns">The amount of "columns" in <paramref name="Grid"/></param>
+        /// <param name="Grid">The "Grid" that contains the input that is to be encrypted</param>
+        /// <returns>The encrypted input.</returns>
+        private static string GetEncryptedValueFromGrid(int columns, List<char[]> Grid)
+        {
+            string result = string.Empty;
+            for (int i = columns - 1; i > -1; i--)
+            {
+                Grid.ForEach(x => result += x[i].ToString());
+            }
+
+            return result;
+        }
+
+        private void decryptBtn_Click(object sender, EventArgs e)
+        {
+            string input = inputTxtBx.Text;
+            for (int i = 0; i < 5; i++)
+            {
+                input = Encrypt(input);
+            }
+            resultTxtBx.Text = input;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
