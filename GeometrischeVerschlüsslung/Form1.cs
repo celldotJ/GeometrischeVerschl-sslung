1977b5f53f24065636c646daece96b878a9898c4
celldotJ <78538876+celldotJ@users.noreply.github.com>
2022-08-30 09:10:09 +0200

    Improved comments on main Program

--- GeometrischeVerschlüsslung/Form1.cs
+++ GeometrischeVerschlüsslung/Form1.cs
@@ -11,10 +11,12 @@ namespace GeometrischeVerschlüsslung
         {
             string input = inputTxtBx.Text;
             input = input.Replace(" ", "").ToUpper();
+
             string result = Encrypt(input);
 
             resultTxtBx.Text = result;
         }
+
         /// <summary>
         /// Encrypts the <paramref name="input"/> using the Geometrische Verschlüsslung
         /// </summary>
@@ -28,27 +30,25 @@ namespace GeometrischeVerschlüsslung
             columns = (int)Math.Ceiling(Math.Sqrt(input.Length));
             rows = (int)Math.Ceiling((decimal)input.Length / columns);
 
-            List<char[]> Grid = CreateGrid(input, columns, rows);
+            List<char[]> grid = CreateGrid(input, columns, rows);
 
-            string result = GetEncryptedValueFromGrid(columns, Grid);
-            return result;
+            return GetEncryptedValueFromGrid(columns, grid);
         }
 
         /// <summary>
-        /// Creates the Grid used for encryption.
+        /// Creates the Grid and fills it with the word that is to be encrypted.
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="columns"></param>
-        /// <param name="rows"></param>
-        /// <returns></returns>
+        /// <param name="input">The value to be encrypted.</param>
+        /// <param name="columns">The amount of columns needed for the Grid.</param>
+        /// <param name="rows">The amount of rows needed for the Grid.</param>
+        /// <returns> A grid with <paramref name="input"/> filled in.</returns>
         private static List<char[]> CreateGrid(string input, int columns, int rows)
         {
             List<char[]> Grid = new List<char[]> { };
-            char[] Chars = input.ToCharArray();
             int progress = 0;
             for (int i = 0; i < rows; i++)
             {
-                char[] row = FillGridRow(input, columns, Chars, ref progress);
+                char[] row = FillGridRow(input, columns, ref progress);
                 Grid.Add(row);
             }
 
@@ -58,21 +58,21 @@ namespace GeometrischeVerschlüsslung
         /// <summary>
         /// Fills a <see cref="char[]"/> with parts of the input string 
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="columns"></param>
-        /// <param name="Chars"></param>
-        /// <param name="progress"></param>
-        /// <returns></returns>
-        private static char[] FillGridRow(string input, int columns, char[] Chars, ref int progress)
+        /// <param name="input">The value to be encrypted</param>
+        /// <param name="columns">The amount of columns in the grid</param>
+        /// <param name="progress">The progress in of the "fill"</param>
+        /// <returns>An array that represents a row in the grid</returns>
+        private static char[] FillGridRow(string input, int columns, ref int progress)
         {
             char[] row = new char[columns];
+            char[] chars = input.ToCharArray();
             for (int z = 0; z < columns; z++)
             {
                 if (progress > input.Length - 1)
                 {
                     break;
                 }
-                row[z] = Chars[progress];
+                row[z] = chars[progress];
                 progress++;
             }
             if (row.Contains('\0'))
@@ -88,6 +88,7 @@ namespace GeometrischeVerschlüsslung
 
             return row;
         }
+
         /// <summary>
         /// Reads the encrypted version of the input from the <paramref name="Grid"/>.
         /// </summary>
